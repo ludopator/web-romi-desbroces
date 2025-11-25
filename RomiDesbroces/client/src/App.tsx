e2896a1d1c1ece5c15236a9717a890eb357c7122
ludopator <pgrosillo@gmail.com>
2025-11-25 19:33:19 +0000

    Fix router base for GitHub Pages

--- RomiDesbroces/client/src/App.tsx
+++ RomiDesbroces/client/src/App.tsx
@@ -1,21 +1,24 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router as WouterRouter } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+
 import Home from "@/pages/Home";
 import AvisoLegal from "@/pages/AvisoLegal";
 import Privacidad from "@/pages/Privacidad";
 import NotFound from "@/pages/not-found";
 
-function AppRouter() {
+function AppRoutes() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/aviso-legal" component={AvisoLegal} />
-      <Route path="/privacidad" component={Privacidad} />
-      <Route component={NotFound} />
-    </Switch>
+    <WouterRouter base="/web-romi-desbroces">
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/aviso-legal" component={AvisoLegal} />
+        <Route path="/privacidad" component={Privacidad} />
+        <Route component={NotFound} />
+      </Switch>
+    </WouterRouter>
   );
 }
 
@@ -24,10 +27,11 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <AppRouter />
+        <AppRoutes />
       </TooltipProvider>
     </QueryClientProvider>
   );
 }
 
 export default App;
+
